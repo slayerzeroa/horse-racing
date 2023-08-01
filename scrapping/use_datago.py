cf78a63f6d9dedbd75c13db01fdb1d271fa5f11f
slayerzeroa <slayerzeroa@gmail.com>
2023-08-01 12:15:41 +0900

    get data from web site and using open api
    +jeju, busan

--- scrapping/use_datago.py
+++ scrapping/use_datago.py
@@ -3,12 +3,14 @@ import json, pandas as pd
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 import pandas as pd
-
+from json_to_pandas import json2pandas
+from list_of_url import urls
 
 encoding_key = "	aJI3VsNcr69ZG2FR5dx8EilBOhjmaX1EiM2d9IxjjbqSIBGliVc7U2hT3%2BbNdINTAIQFTzdMNfpD%2FaemMvFyVQ%3D%3D"
 
 url = "http://apis.data.go.kr/B551015/API214_1/RaceDetailResult_1"
 
+pd.set_option('display.max_columns', None)
 
 params = {
     "serviceKey": encoding_key,
@@ -19,13 +21,16 @@ params = {
     "rc_no": 3,
     "_type": "json"
 }
-
-r = urlopen(Request(url + '?' + urlencode(params)))
-# r = requests.get(url, params=params)
-json_api = r.read().decode('utf-8')
-
-json_file = json.loads(json_api)
-print(json_file)
-
-df = pd.json_normalize(json_file['response']['body']['items']['item'])
+# #
+# # r = urlopen(Request(url + '?' + urlencode(params)))
+# # # r = requests.get(url, params=params)
+# # json_api = r.read().decode('utf-8')
+# #
+# # json_file = json.loads(json_api)
+# # print(json_file)
+# #
+# # df = pd.json_normalize(json_file['response']['body']['items']['item'])
+# # print(df)
+#
+df = json2pandas(url, params)
 print(df)
