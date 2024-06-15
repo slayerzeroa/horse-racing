3bed735c8a5d4425022452744423478001c54edc
slayerzeroa <slayerzeroa@gmail.com>
2024-06-15 23:06:21 +0900

    fix: fix error

--- main.py
+++ main.py
@@ -5,31 +5,23 @@ import horse_racing_chatbot as chatbot
 from horse_xgboost_prob import *
 import asyncio
 
-
-start = '20240607'
-end = '20240609'
-
-df = get_predict_data(start, end)
-print(df)
+#
+# start = '20240607'
+# end = '20240609'
+#
+# df = get_predict_data(start, end)
+# print(df)
 
 def main():
     start, end = tools.get_start_end()
-    start = '20240607'
-    end = '20240609'
-    try:
-        update_rcPlan()
-    except:
-        pass
-
-    try:
-        update_rcResult(start, end)
-    except:
-        pass
-
-    try:
-        update_modelData(start, end)
-    except:
-        pass
+    start = '20240615'
+    end = '20240615'
+    update_rcPlan(start, end)
+    print('rcPlan updated')
+    update_rcResult(start, end)
+    print('rcResult updated')
+    update_modelData(start, end)
+    print('modelData updated')
 
     try:
         df = get_predict_data(start, end)
@@ -41,5 +33,14 @@ def main():
         pass
 
 
-if __name__ == '__main__':
-    main()
+main()
+
+while True:
+    ## 매일 00시 01분에 실행
+    now = datetime.datetime.now()
+    if now.hour == 9 and now.minute == 1:
+        try:
+            main()
+            print('horse prediction completed')
+        except Exception as e:
+            print(e)
